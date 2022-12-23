41a3b582f905dabe4c069cd1ff020529e490cdaa
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-12-23 20:04:30 +0900

    detail페이지 작업 시작

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ReactPlayer from "react-player";
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const id = useParams();
+  useEffect(() => {}, []);
+
+  return (
+    <section>
+      <article>
+        <ReactPlayer
+          url=""
+          poster=""
+          width="100%"
+          height="100%"
+          playing={true}
+          muted={true}
+          controls={true}
+          light={false}
+          pip={true}
+          style
+        />
+      </article>
+    </section>
+  );
 };
 
 export default Detail;
