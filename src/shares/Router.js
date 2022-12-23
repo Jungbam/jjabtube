30417dcd0739810fe183caa0469121bd9dc7cd23
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-12-23 16:55:36 +0900

    페이지 라우팅 수정

--- src/shares/Router.js
+++ src/shares/Router.js
@@ -2,7 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Error404 from "../pages/404/Error404";
+import Detail from "../pages/detail/Detail";
 import Intro from "../pages/intro/Intro";
+import Live from "../pages/live/Live";
 import SignIn from "../pages/sign/SignIn";
 import SignUp from "../pages/sign/SignUp";
 
@@ -13,8 +16,11 @@ const Router = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Intro />} />
+          <Route path="/detail/:videoId" element={<Detail />} />
+          <Route path="/live" element={<Live />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/*" element={<Error404 />} />
         </Routes>
         <Footer />
       </BrowserRouter>
