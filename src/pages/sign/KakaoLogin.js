a25a14cdbfb0c21fb4de855ab7ccb66f58e4ba11
hanul-dev <hanul.developer@gmail.com>
2022-12-26 00:34:46 +0900

    fix variable name

--- src/pages/sign/KakaoLogin.js
+++ src/pages/sign/KakaoLogin.js
@@ -1,7 +1,7 @@
-const CLIENT_ID = process.env.REACT_APP_KAKAO_ID;
+const CLIENT_ID = process.env.REACT_APP_KAKAO_LOCAL;
 const REDIRECT_URI = "http://localhost:3000/";
 
 // const CLIENT_ID = process.env.REACT_APP_KAKAO;
-// const REDIRECT_URI = "https://sparta-syk.site/";
+// const REDIRECT_URI = "https://sparta-syk.site/login/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
